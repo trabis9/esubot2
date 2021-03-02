de8e829ac87469f454090a0c892eaed593d736a5
trabis9 <73802804+trabis9@users.noreply.github.com>
2021-03-02 11:43:56 -0600

    Delete plugins directory

--- plugins/creator.js
+++
@@ -1,9 +0,0 @@
-let handler = function (m) {
-  this.sendContact(m.chat, '+529984907794', '👑Samu330👑', m)
-}
-handler.help = ['owner', 'creator']
-handler.tags = ['info']
-
-handler.command = /^(owner|creator)$/i
-
-module.exports = handler
