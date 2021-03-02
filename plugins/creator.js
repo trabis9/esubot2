658b9f47465391dc9bbffc4ddf64369a51a1f35a
trabis9 <73802804+trabis9@users.noreply.github.com>
2021-03-02 11:43:17 -0600

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,9 @@
+let handler = function (m) {
+  this.sendContact(m.chat, '+529984907794', '👑Samu330👑', m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
